9460fa6a3b775591e7e645a19f0af913620fb915
KhoalaS <khoalekappa@gmail.com>
2025-01-18 14:57:22 +0100

    Bump version 2024.06

--- app/src/main/java/ml/docilealligator/infinityforreddit/utils/APIUtils.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/utils/APIUtils.java
@@ -44,8 +44,8 @@ public class APIUtils {
     public static final String AUTHORIZATION_KEY = "Authorization";
     public static final String AUTHORIZATION_BASE = "bearer ";
     public static final String USER_AGENT_KEY = "User-Agent";
-    public static final String USER_AGENT = "Reddit/Version 2024.05.0/Build 1403584/Android 11";
-    public static final String VIDEO_USER_AGENT = "RedditVideo/Version 2024.05.0/Build 1403584/Android 11";
+    public static final String USER_AGENT = "Reddit/Version 2024.06.0/Build 1418489/Android 11";
+    public static final String VIDEO_USER_AGENT = "RedditVideo/Version 2024.06.0/Build 1418489/Android 11";
 
     public static final String GRANT_TYPE_KEY = "grant_type";
     public static final String GRANT_TYPE_CLIENT_CREDENTIALS = "client_credentials";
